5ebde294f1dddf1b52a6a7647431c0455750f9c1
Rishikesh Rachchh <rishikeshrachchh@gmail.com>
2021-05-02 01:26:26 +0400

    Linux setup fix

--- setup.sh
+++ setup.sh
@@ -3,5 +3,7 @@ sudo apt install python3-venv > /dev/null
 echo "Creating virtual environment..."
 python3 -m venv venv1 > /dev/null
 source venv1/bin/activate
+echo "Ensuring pip is the latest version..."
+python3 -m pip install --upgrade pip > /dev/null
 echo "Installing requirements for the virtual environment..."
 python3 -m pip install -r requirements.txt > /dev/null
