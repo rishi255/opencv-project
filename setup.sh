68c8a0049628b55730968e902d2c69143b907e91
Rishikesh Rachchh <rishikeshrachchh@gmail.com>
2021-05-02 00:52:07 +0400

    Added setup files

---
+++ setup.sh
@@ -0,0 +1,9 @@
+echo "Installing python3-venv package..."
+sudo apt install python3-venv > /dev/null
+echo "Creating virtual environment..."
+python3 -m venv venv1 > /dev/null
+source venv1/bin/activate
+echo "Installing requirements for the virtual environment..."
+python3 -m pip install -r requirements.txt > /dev/null
+echo "Launching Flask server..."
+flask run
